ec730c668a9441cfe089c2160971bc7ebcd58af0
ilhan <ilhanahmed383@gmail.com>
2024-03-17 17:25:11 +0500

    first commit

---
+++ about.php
@@ -0,0 +1,188 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/23.png" alt="">
+      </div>
+
+      <div class="content">
+         <h3>Developer's Message:</h3>
+         <p>Hey There ! I'm Ilhan Ahmed. A Student of BS in Computer Science from NEDUET. I love designing websites and exploring new things. Learning new things is my hobby.</p>
+
+         <a href="contact.php" class="btn">Contact Us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+   
+   <h1 class="heading">Client's Reviews.</h1>
+
+   <div class="swiper reviews-slider">
+
+   <div class="swiper-wrapper">
+
+      <div class="swiper-slide slide">
+         <img alt="Not Found">
+         <p>Been using their services for quite a bit and have never had an issue with the quality of their products. Online e-products working great as well. Only issue I have is they usually deliver when I'm a little caught up, though I've set a preferred delivery time. Everything else has been good.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3> Ammar Ahmed</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img  alt="Not Found">
+         <p>It is the first online services in Nepal which we can trust completely.I always unbox making a video and instantly complain if there's anything wrong. Sometimes even don't need to return the item and they process the refund. KinBech do heavy fine to sellers who send wrong products thats why its platform getting better day by day.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Hamza Ali</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img alt="Not Found">
+         <p>KinBech is great if you choose good sellers . A variety of required item available . Customers can return and refund full amount within 7 days easily . KinBech is boosting eCommerce business in Kathmandu.It provides great opportunity to sale items online with ease.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Shammas Qazi</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img  alt="Not Found">
+         <p>Using KinBech for online shopping from almost 3 years. Outstanding experience with them. Game vouchers and pick up point as delivery with 0 shipping charges are super saving services.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Ahmed Baloch</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img  alt="Not Found">
+         <p>I have been using their services for the last 2 years and I have found them extremely reliable.Their return policy is what gives you an extra layer of reliance and peace of mind. In case the product doesn't meet your expectations or if there is any fault in it. then you can return the product within seven days from the date of delivery.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Haris Ali</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img alt="Not Found">
+         <p>KinBech is cool! I have ordered hundreds of products from it and never got any scam. It delivers products in time with out delay. Packaging of products are strong and delivery rates are too low. Just amazing Website will keep shopping from KinBech.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Zain Ali</h3>
+      </div>
+
+   </div>
+
+   <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+        slidesPerView:1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+</script>
+
+</body>
+</html>
---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel:9800000000"><i class="fas fa-phone"></i> +92 123 456 7890</a>
+         <a href="tel:9900000000"><i class="fas fa-phone"></i> +92 123 456 7890</a>
+         <a href="mailto:harshchy143@gmail.com"><i class="fas fa-envelope"></i> ilhanahmed383@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Karachi, Pakistan </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://web.facebook.com/ilhan.ahmed.338/" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://www.linkedin.com/in/ilhan-ahmed/" target="_blank"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>ILHAN AHMED</span> | all rights reserved!</div>
+
+</footer>
